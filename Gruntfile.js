ee35ffc63d28a80f1d2e9dde23290c1ede0c1dd9
Israel Sanchez <israel.sanchez@eigital.com>
2018-01-16 16:22:32 -0600

    Basic server Signed-off-by:Israel Sanchez <israel.sanchez@eigital.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,21 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         
         jshint: { // se configura jshint
+            options: [], // todas las tareas tienen opciones
             // esto ejecuta la tare jshint:all
             all: ['Gruntfile.js', 'app/**/*.js'] // app/ todos los archivos .js 
+        },
+        
+        concat:{
+            options: {
+                separator: '',
+                stripBanners: true,
+                banner: '/*!\nConcat: <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */',
+            },
+            dist:{
+                src: ['app/controllers/*.js'],
+                dest: 'app/controllers/ctrls.js'
+            }
         },
         
         watch: {
@@ -19,22 +32,40 @@ module.exports = function (grunt) {
         
         uglify: { // ver más opciones
             options: {
-              banner: '/*!\n<%= pkg.name %>\nv: <%= pkg.version %>\nBy: <%= pkg.author %>\n<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+              banner: '/*!\nUglify  <%= pkg.name %>\nv: <%= pkg.version %>\nBy: <%= pkg.author %>\n<%= grunt.template.today("yyyy-mm-dd") %> */\n',
               footer: '\n/*Fin chido*/'
             },
             build: {
               src: 'app/**/*.js', // todos los archivos
               dest: 'build/<%= pkg.name %>.min.js' // arma un solo archivo
             }
-          }
+          },
+          
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: {
+                        path: 'app',
+                        options: {
+                            index: 'index.html',
+                            maxAge: 300000
+                        }
+                    },
+                    keepalive: true
+                }
+            }
+        }
     });
     
     // después de instalar el paquete, lo ponemos aquí para que sea cargado
     grunt.loadNpmTasks('grunt-contrib-jshint'); // <-- libreria instalñada con npm
-    grunt.loadNpmTasks('grunt-contrib-watch'); // <-- libreria instalñada con npm
-    grunt.loadNpmTasks('grunt-contrib-uglify'); // <-- libreria instalñada con npm
+    grunt.loadNpmTasks('grunt-contrib-watch'); 
+    grunt.loadNpmTasks('grunt-contrib-uglify'); 
+    grunt.loadNpmTasks('grunt-contrib-concat'); 
+    grunt.loadNpmTasks('grunt-contrib-connect');
     
     // Default task(s).
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
     
 };
---
+++ app/controllers/ctrls.js
@@ -0,0 +1,14 @@
+/*!
+Concat: demo - v1.0.0 - 2018-01-16 */var funca = function(){
+    console.log('Soy A');
+};
+
+var funcb = function(a){
+    console.log('Soy B');
+    if(typeof a === 'function'){
+        a(); // se tiene que mandar llamar como método
+    }
+    else{
+        console.log('No es funcion');
+    }
+};
